6e29e5765f7c43d12927173c044a38afa6f9c27f
Goutham K C <gouthamkc153@gmail.com>
2026-02-26 15:45:44 +0530

    client side

---
+++ client/src/app.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'preact/hooks'
+import preactLogo from './assets/preact.svg'
+import viteLogo from '/vite.svg'
+import './app.css'
+
+export default function App() {
+  return (
+    <div className="min-h-screen bg-black text-white flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-green-400">
+        GhostLearn Running 🚀
+      </h1>
+    </div>
+  )
+}
